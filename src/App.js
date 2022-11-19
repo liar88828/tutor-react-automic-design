3f7b4905900b93ac44cda111f81c0c0faaedc5f7
liar780@gmail.com <liar780@gmail.com>
2022-11-19 16:48:01 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { UseImperativeHandleTutor } from "./tutor/useImperatifTutor/UseImperativeHandleTutor";
+// import { UseRefTutor } from "./tutor/UseRefTutor";
+// import { UseLayoutEffectTutor } from "./tutor/UseLayoutEffectTutor";
+// import { UseStateTutor } from "./tutor/UseStateTutor";
+// import { UseReducerTutor } from "./tutor/UseReducerTutor";
+// import { UseEffectTutor } from "./tutor/UseEffectTutor";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <UseStateTutor /> */}
+      {/* <UseReducerTutor /> */}
+      {/* <UseEffectTutor /> */}
+      {/* <UseRefTutor /> */}
+      {/* <UseLayoutEffectTutor /> */}
+      <UseImperativeHandleTutor />
     </div>
   );
 }
