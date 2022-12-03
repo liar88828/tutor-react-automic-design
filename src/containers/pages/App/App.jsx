7fe661f8db5550312b953a50490f38d2ff4e3d36
liar780@gmail.com <liar780@gmail.com>
2022-12-03 13:06:11 +0700

    save email firebase,

--- src/containers/pages/App/App.jsx
+++ src/containers/pages/App/App.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Login from "../Login/Login";
+import Dashboard from "../Dasboard/Dashboard";
+import Register from "../Register/Register";
 
 // import { Home } from "./container/Home/Home";
 
@@ -6,10 +10,34 @@ function App() {
 
 
 	return (
-		<div className='app'>
-			<h1>hahahah</h1>
-		</div>
+		<BrowserRouter>
+
+			<div className='app'>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/Dashboard  '>Dashboard</Link>
+						</li>
+						<li>
+							<Link to='/Register'>Register</Link>
+						</li>
+					</ul>
+				</nav>
+				< Routes>
+					<Route path='/' exact element={<Login/>}/>
+					<Route path='/Dashboard' element={<Dashboard/>}/>
+					<Route path='/Register' element={<Register/>}/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+
 	);
+
+
 }
+
 
 export default App;
---
+++ src/containers/pages/Register/Register.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import './register.scss'
+import {apps} from '../../../config/firebase/index'
+import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+
+class Register extends Component {
+
+
+	state = {
+		email: '',
+		password: ''
+	}
+
+	handleChangeText = (e) => {
+		// console.log(e.target.id)// log id
+		this.setState({
+			[e.target.id]: e.target.value,
+		})
+	}
+
+	handleChangeSubmit = () => {
+		// console.log("email : ", this.state.email)
+		// console.log('password : ', this.state.password)
+
+		const {email, password} = this.state
+		const auth = getAuth(apps);
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				// const user = userCredential.user;
+				console.log('success :', userCredential)
+
+
+				// ...
+			})
+			.catch((error) => {
+				console.log(error)
+				// const errorCode = error.code;
+				// const errorMessage = error.message;
+				// ..
+			});
+	}
+
+	render() {
+		return (
+			<div className='auth-container'>
+				<div className='auth-card'>
+					<h1>Register</h1>
+					<h1 className='aunt-title'>Register Page </h1>
+					<input type='text'
+					       id='email'
+					       placeholder='Email'
+					       className='input'
+					       onChange={this.handleChangeText}
+
+					/>
+					<input type='password'
+					       id='password'
+					       placeholder='Password'
+					       className='input'
+					       onChange={this.handleChangeText}
+
+					/>
+					<button
+						className='btn'
+						onClick={this.handleChangeSubmit}>Register
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Register
